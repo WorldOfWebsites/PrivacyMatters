b6ba8e972d73af26720b7fbb156ba4d1701635e6
sweeper3000 <spiketwofish@protonmail.com>
2016-07-20 15:02:48 -0400

    Add search text for Silence

--- tipuesearch/tipuesearch_content.js
+++ tipuesearch/tipuesearch_content.js
@@ -2,7 +2,7 @@ var tipuesearch ={"pages":[
 	{"title": "Privacy", "text": "A page explaining why privacy matters more than ever today.", "tags": "privacy", "url": "privacy/index.html"},
 	{"title": "Tools", "text": "A list of tools you can use to protect your privacy online.", "tags": "tools, encryption, crypto, cryptography", "url": "tools/index.html"},
 	{"title": "Signal Private Messenger", "text": "A page about Open Whisper System's app Signal, an easy to use encrypted chat app.", "tags": "encryption, crypto, cryptography, private chat, signal, encrypted chat", "url": "tools/signal/index.html"},
-	{"title": "Silence", "text": "", "tags": "encryption, crypto, cryptography, private chat, silence, encrypted chat, encrypted sms, encrypted mms", "url": "tools/silence/index.html"},
+	{"title": "Silence", "text": "A page about Silence, a fork of TextSecure (now Signal) that retains SMS encryption.", "tags": "encryption, crypto, cryptography, private chat, silence, encrypted chat, encrypted sms, encrypted mms", "url": "tools/silence/index.html"},
 	{"title": "Tor", "text": "A page about Tor, an anonymous way to access the internet.", "tags": "tor, the onion router, anonymous web, crypto, encryption, cryptography", "url": "tools/tor/index.html"},
 	{"title": "PGP", "text": "A page about the PGP (Pretty Good Privacy) encryption software.", "tags": "crypto, cryptography, pgp, pretty good privacy, email encryption", "url": "tools/pgp/index.html"},
 	{"title": "Cryptography", "text": "A page explaining cryptography.", "tags": "encryption, crypto, cryptography,", "url": "crypto/index.html"},
