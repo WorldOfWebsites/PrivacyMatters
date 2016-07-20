44e85fdb32c370c9b9ddc3036061de08535d63fd
sweeper3000 <spiketwofish@protonmail.com>
2016-07-19 22:04:27 -0400

    Begin addition of tipuesearch search engine

---
+++ tipuesearch/tipuesearch_content.js
@@ -0,0 +1,10 @@
+var tipuesearch ={"pages":[
+	{"title": "Privacy", "text": "", "tags": "privacy", "url": "privacy/index.html"},
+	{"title": "Tools", "text": "", "tags": "tools, encryption, crypto, cryptography", "url": "tools/index.html"},
+	{"title": "Signal Private Messenger", "text": "", "tags": "encryption, crypto, cryptography, private chat, signal, encrypted chat", "url": "tools/signal/index.html"},
+	{"title": "Silence", "text": "", "tags": "encryption, crypto, cryptography, private chat, silence, encrypted chat, encrypted sms, encrypted mms", "url": "tools/silence/index.html"},
+	{"title": "Tor", "text": "", "tags": "tor, the onion router, anonymous web, crypto, encryption, cryptography", "url": "tools/tor/index.html"},
+	{"title": "PGP", "text": "", "tags": "crypto, cryptography, pgp, pretty good privacy, email encryption", "url": "tools/pgp/index.html"},
+	{"title": "Cryptography", "text": "", "tags": "encryption, crypto, cryptography,", "url": "crypto/index.html"},
+	{"title": "Media for the Layperson", "text": "", "tags": "media for the layperson, media, videos, external, crypto, encryption, cryptography", "url": "tools/signal/index.html"},
+]};
