d11cc7440b4bd13ea3f69538042aba56b4eba002
sweeper3000 <spiketwofish@protonmail.com>
2016-07-20 14:56:14 -0400

    Add text to search results and keywords

--- tipuesearch/tipuesearch_content.js
+++ tipuesearch/tipuesearch_content.js
@@ -1,10 +1,10 @@
 var tipuesearch ={"pages":[
-	{"title": "Privacy", "text": "", "tags": "privacy", "url": "privacy/index.html"},
-	{"title": "Tools", "text": "", "tags": "tools, encryption, crypto, cryptography", "url": "tools/index.html"},
-	{"title": "Signal Private Messenger", "text": "", "tags": "encryption, crypto, cryptography, private chat, signal, encrypted chat", "url": "tools/signal/index.html"},
+	{"title": "Privacy", "text": "A page explaining why privacy matters more than ever today.", "tags": "privacy", "url": "privacy/index.html"},
+	{"title": "Tools", "text": "A list of tools you can use to protect your privacy online.", "tags": "tools, encryption, crypto, cryptography", "url": "tools/index.html"},
+	{"title": "Signal Private Messenger", "text": "A page about Open Whisper System's app Signal, an easy to use encrypted chat app.", "tags": "encryption, crypto, cryptography, private chat, signal, encrypted chat", "url": "tools/signal/index.html"},
 	{"title": "Silence", "text": "", "tags": "encryption, crypto, cryptography, private chat, silence, encrypted chat, encrypted sms, encrypted mms", "url": "tools/silence/index.html"},
-	{"title": "Tor", "text": "", "tags": "tor, the onion router, anonymous web, crypto, encryption, cryptography", "url": "tools/tor/index.html"},
-	{"title": "PGP", "text": "", "tags": "crypto, cryptography, pgp, pretty good privacy, email encryption", "url": "tools/pgp/index.html"},
-	{"title": "Cryptography", "text": "", "tags": "encryption, crypto, cryptography,", "url": "crypto/index.html"},
-	{"title": "Media for the Layperson", "text": "", "tags": "media for the layperson, media, videos, external, crypto, encryption, cryptography", "url": "mediaforthelayperson/index.html"},
+	{"title": "Tor", "text": "A page about Tor, an anonymous way to access the internet.", "tags": "tor, the onion router, anonymous web, crypto, encryption, cryptography", "url": "tools/tor/index.html"},
+	{"title": "PGP", "text": "A page about the PGP (Pretty Good Privacy) encryption software.", "tags": "crypto, cryptography, pgp, pretty good privacy, email encryption", "url": "tools/pgp/index.html"},
+	{"title": "Cryptography", "text": "A page explaining cryptography.", "tags": "encryption, crypto, cryptography,", "url": "crypto/index.html"},
+	{"title": "Media for the Layperson", "text": "A list of media to explain related concepts relevent to this issue.", "tags": "media for the layperson, media, videos, external, crypto, encryption, cryptography, youtube, last week tonight", "url": "mediaforthelayperson/index.html"},
 ]};
