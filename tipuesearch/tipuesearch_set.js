e6668e4f006814c8d44265c13aa2deef5c47e9fe
sweeper3000 <spiketwofish@protonmail.com>
2016-07-23 23:59:00 -0400

    Remove beginning addresses

--- tipuesearch/tipuesearch_set.js
+++ tipuesearch/tipuesearch_set.js
@@ -8,7 +8,7 @@ http://www.tipue.com/search
 
 // Site index
 
-var tipuesearch_pages = ["https://worldofwebsites.github.io/PrivacyMatters/", "https://worldofwebsites.github.io/PrivacyMatters/tools/index.html/", "https://worldofwebsites.github.io/PrivacyMatters/privacy/index.html/", "https://worldofwebsites.github.io/PrivacyMatters/mediaforthelayperson/index.html/", "https://worldofwebsites.github.io/PrivacyMatters/crypto/index.html/", "https://worldofwebsites.github.io/PrivacyMatters/tools/signal/index.html/", "https://worldofwebsites.github.io/PrivacyMatters/tools/pgp/index.html/", "https://worldofwebsites.github.io/PrivacyMatters/tools/silence/index.html/", "https://worldofwebsites.github.io/PrivacyMatters/tools/tox/index.html/", "https://worldofwebsites.github.io/PrivacyMatters/tools/tor/index.html/",];
+var tipuesearch_pages = ["https://worldofwebsites.github.io/PrivacyMatters/", "tools/index.html/", "/privacy/index.html/", "mediaforthelayperson/index.html/", "crypto/index.html/", "tools/signal/index.html/", "tools/pgp/index.html/", "tools/silence/index.html/", "tools/tox/index.html/", "tools/tor/index.html/",];
 
 /*
 Stop words
